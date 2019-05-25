67a67d3a6b5c05db628dda66b9095cdf6c3f5d8b
Mark Meehan <mtimothy38@gmail.com>
2019-05-25 17:09:06 +0100

    Improved deleting function

--- script.py
+++ script.py
@@ -11,32 +11,38 @@ SCOPES = ['https://www.googleapis.com/auth/calendar'] #READONLY scope
 
 
 def setEvent(service):
+
+    GMT_0FF = '+00:00'
+    name = raw_input("What name do you wish your event to have?")
+    dayTime = raw_input("What day/time is your event? Please enter (YYYY)-(MM)-(DD)T(HH):(MM):(SS)")
+
     EVENT = {
-        'summary': 'teste',
-        'start': {'date' : '2019-06-21'},
-        'end' : {'date' : '2019-06-22'},
+        'summary': '%s' % name,
+        # 'start.timeZone' : 'Europe/Lisbon',
+        'start': {'dateTime' : '%s%s' % (dayTime, GMT_0FF)},
+        # 'end.timeZone' : 'Europe/Lisbon',
+        'end' : {'dateTime' : '%s%s' % (dayTime, GMT_0FF)},
     }
 
     e = service.events().insert(calendarId = 'primary', sendNotifications = False, body = EVENT).execute()
 
     print('****%r event added!****' % e['summary'].encode('utf-8'))
-    print('Start: %s' % e['start']['date'])
-    print('End: %s' % e['end']['date'])
+    print('Start: %s' % e['start']['dateTime'])
+    print('End: %s' % e['end']['dateTime'])
 
 
 def deleteEvent(service):
-    # print('Event name?')
-    # name = input()
+    print('Event name?')
+    name = raw_input()
 
     events = getEvents(service, 100)
 
     for event in events:
-        if event['summary'] == 'teste':
+        if event['summary'] == name:
             id = event['id']
             print(id)
             e = service.events().delete(calendarId='primary', eventId=id).execute()
-
-    # print('****%r event deleted!****' % e['summary'].encode('utf-8'))
+            # print('****%r event deleted!****' % e['summary'].encode('utf-8'))
 
 
 
@@ -47,8 +53,6 @@ def printEvents(events, numEvents):
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
         eventCounter = eventCounter + 1
-        if event['summary'] == 'teste':
-            print('FOUND TESTE')
 
     if eventCounter < numEvents:
         print("It seems you only had %s events planned" % eventCounter)
@@ -59,7 +63,6 @@ def getEvents(service, numEvents):
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming %s events:' % numEvents)
     events_result = service.events().list(
                                         calendarId='primary', 
                                         timeMin=now,
@@ -72,7 +75,6 @@ def getEvents(service, numEvents):
         print('No upcoming events found.')
 
     return events
-
 
 
 
@@ -101,7 +103,6 @@ def main():
 
 
     service = build('calendar', 'v3', credentials=creds)
-
 
     run = True
 
